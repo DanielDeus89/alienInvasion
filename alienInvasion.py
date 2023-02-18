ca90f907682cb936edfb6dab26cdf2c96fa183aa
DanielDeus <danielsilvadeus@gmail.com>
2023-02-18 15:40:10 -0300

    alienInvasion.py

---
+++ alienInvasion.py
@@ -0,0 +1,31 @@
+import sys
+import pygame
+
+from settings import Settings
+
+class AlienInvasion:
+    def __init__(self):
+        pygame.init()
+        self.settings = Settings()
+
+        self.screen = pygame.display.set_mode((
+            self.settings.screen_width, self.settings.screen_height))
+        pygame.display.set_caption('Alien Invasion')
+
+    
+
+    def run_game(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+
+            self.screen.fill(self.settings.bg_color)
+
+                
+            pygame.display.flip()
+
+
+if __name__ == '__main__':
+    ai = AlienInvasion()
+    ai.run_game()
